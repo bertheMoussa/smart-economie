f8489db619785e02619a5c873beb628e87cbb371
berthemoussa102 <berthemoussa102@gmail.com>
2026-03-08 15:25:31 +0100

    initialisation du projet

---
+++ src/test/java/com/smart_economie/smart_economie/SmartEconomieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_economie.smart_economie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEconomieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
